a49178721f1522c0a42e6855654138c27b899efb
himansumuduli <97511041+himansumuduli@users.noreply.github.com>
2024-07-15 21:01:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function fibonacci(num) {
 		return 0;
 	}
 	else if(num === 1){
-		return 0;
+		return 1;
 	}
 	let a=0;
 	let b=1;
-	for(int i =2;i<=num;i++){
+	for(let i =2;i<=num;i++){
 		let temp=a+b;
 		a=b;
 		b=temp;
